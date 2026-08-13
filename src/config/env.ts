f5df6fb0cfb4968f7f2123beb998a1697458dcb7
anwjrdid <yangchaehyeon67@gmail.com>
2026-08-13 18:56:46 +0900

    [feat]: Demo POS QR 발급 흐름 구현

--- src/config/env.ts
+++ src/config/env.ts
@@ -13,6 +13,7 @@ export const env = {
   appMode: resolveAppMode(),
   apiBaseUrl: viteEnv.VITE_API_BASE_URL ?? '',
   useCustomerApi: viteEnv.VITE_USE_CUSTOMER_API === '1',
+  customerAppUrl: viteEnv.VITE_CUSTOMER_APP_URL ?? '',
   demoStoreId: viteEnv.VITE_DEMO_STORE_ID ?? '',
   demoProductId: viteEnv.VITE_DEMO_PRODUCT_ID ?? '',
   adminUsername: viteEnv.VITE_ADMIN_USERNAME ?? (viteEnv.DEV ? 'demo-owner' : ''),
